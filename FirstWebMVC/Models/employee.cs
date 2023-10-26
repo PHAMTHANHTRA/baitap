04c80484b2d666c051a44264d0121da57ea79b8e
PHAMTHANHTRA <142491459+PHAMTHANHTRA@users.noreply.github.com>
2023-10-26 13:00:55 +0700

    Create employee.cs

---
+++ FirstWebMVC/Models/employee.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+//Pham Thanh Tra - 2021050646
+namespace FirstWebMVC.Models
+{
+    public class Employee 
+    {
+        public string FullName { get; set; }
+        public int HeSoLuong { get; set; } 
+        public int LuongCB { get; set; }
+        public int PhuCap { get; set; }
+    //NPham Thanh Tra - 2021050646
+    }
+}
