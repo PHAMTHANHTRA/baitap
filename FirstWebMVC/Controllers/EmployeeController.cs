711c505ee2e79b5803f4f249f19128cefaf36c34
PHAMTHANHTRA <142491459+PHAMTHANHTRA@users.noreply.github.com>
2023-10-31 00:59:34 +0700

    EmployeeController.cs

--- FirstWebMVC/Controllers/EmployeeController.cs
+++ FirstWebMVC/Controllers/EmployeeController.cs
@@ -7,22 +7,22 @@ using FirstWebMVC.Models;
 
 namespace FirstWebMVC.Controllers
 {
-//Pham Thanh Tra - 2021050543
+//Pham Thanh Tra - 2021050646
     public class EmployeeController : Controller
     {
          public IActionResult Index()
             {
                 return View();
-//Pham Thanh Tra - 2021050543
+//Pham Thanh Tra - 2021050646
             }
             [HttpPost]
          public IActionResult Index(Employee emp)
             {
-//Pham Thanh Tra - 2021050543
+//Pham Thanh Tra - 2021050646
                 string str = emp.FullName + "_" + emp.EmployeeID + "_" + emp.SoDienThoai +"_"+ emp.Address; 
                 ViewBag.KetQua = str;
                 return View();
-//Pham Thanh Tra - 2021050543
+//Pham Thanh Tra - 2021050646
             }
     }
 }
