cc967a45acb896b7eadc2822949494c30d33f5c7
PHAMTHANHTRA <142491459+PHAMTHANHTRA@users.noreply.github.com>
2023-10-30 20:23:40 +0700

    Create HeThongPhanPhoi.cs

---
+++ FirstWebMVC/Models/HeThongPhanPhoi.cs
@@ -0,0 +1,12 @@
+namespace DemoMVC.Models
+{
+//Pham Thanh Tra - 2021050646
+    public class HeThongPhanPhoi
+    {
+//Pham Thanh Tra - 2021050646
+        public string MaHTPP { get; set; }
+        public string TenHTPP { get; set; }
+    }
+}
+
+//Pham Thanh Tra - 2021050646
