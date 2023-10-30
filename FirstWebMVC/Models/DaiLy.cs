e29ba42425fd5bb8785589d1de72e2297845efa0
PHAMTHANHTRA <142491459+PHAMTHANHTRA@users.noreply.github.com>
2023-10-30 20:17:59 +0700

    Create DaiLy.cs

---
+++ FirstWebMVC/Models/DaiLy.cs
@@ -0,0 +1,15 @@
+namespace FirstWebMVC.Models
+{
+    public class DaiLy : HeThongPhanPhoi
+    {
+//Pham Thanh Tra - MSV : 2021050646
+        public string MaDaiLy { get; set; }
+        public string TenDaiLy { get; set; }
+        public string DiaChi { get; set; }
+//Pham Thanh Tra - MSV : 2021050646
+        public string NguoiDaiDien { get; set; }
+        public string DienThoai { get; set; }
+        
+    }
+}
+//Pham Thanh Tra - MSV : 2021050646
