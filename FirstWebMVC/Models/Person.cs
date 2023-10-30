02bbc7ed244b3d90eccde8c5e2c4402ced4c04b7
PHAMTHANHTRA <142491459+PHAMTHANHTRA@users.noreply.github.com>
2023-10-30 20:25:08 +0700

    Create Person.cs

---
+++ FirstWebMVC/Models/Person.cs
@@ -0,0 +1,11 @@
+namespace DemoMVC.Models
+{
+    //Pham Thanh Tra - MSV : 2021050646
+    public class Person
+    {
+        public string PersonID { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+    }
+}
+  //Pham Thanh Tra - MSV : 2021050646
